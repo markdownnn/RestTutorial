3e5658102d0a3d1846c5ab39555a7483ebd393d8
Hyeok <mh960110@gmail.com>
2021-01-03 02:00:52 +0900

    Add user related serializer and view

--- snippets/views.py
+++ snippets/views.py
@@ -1,6 +1,7 @@
 from snippets.models import Snippet
-from snippets.serializers import SnippetSerializer
+from snippets.serializers import SnippetSerializer, UserSerializer
 from rest_framework import generics
+from django.contrib.auth.models import User
 
 # Create your views here.
 
@@ -13,3 +14,13 @@ class SnippetList(generics.ListCreateAPIView):
 class SnippetDetail(generics.RetrieveUpdateDestroyAPIView):
     queryset = Snippet.objects.all()
     serializer_class = SnippetSerializer
+
+
+class UserList(generics.ListAPIView):
+    queryset = User.objects.all()
+    serializer_class = UserSerializer
+
+
+class UserDetail(generics.RetrieveAPIView):
+    queryset = User.objects.all()
+    serializer_class = UserSerializer
